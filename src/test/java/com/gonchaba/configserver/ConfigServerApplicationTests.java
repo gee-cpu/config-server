48d370f8ac9a871f8fafa2f678057ea57680f8d0
george.o <ggonchaba@gmail.com>
2024-02-07 21:01:58 +0300

    fixing tests

--- src/test/java/com/gonchaba/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/gonchaba/configserver/ConfigServerApplicationTests.java
@@ -2,11 +2,14 @@ package com.gonchaba.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(properties = {
 		"spring.profiles.active=test",
 
 })
+@TestPropertySource(locations = "classpath:application-test.yaml")
+
 public class ConfigServerApplicationTests {
 
 	@Test
